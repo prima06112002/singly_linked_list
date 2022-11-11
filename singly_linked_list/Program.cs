97603f4c5daef6dc863632024085c639eac23613
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-11 17:21:31 +0700

    add public void addNote

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -8,19 +8,31 @@ namespace singly_linked_list
 {
     // each node consist of the information part and lik to to the next mode
 
-    class node
+    class Node
     {
         public int rollNumber;
         public string name;
-        public node next;
+        public Node next;
     }
     class List
     {
-        node START;
+        Node START;
 
         public List()
         {
             START = null;
+        }
+        
+        public void addNote() // add a node in the list
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student : ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.name = nm;
         }
 
     }
